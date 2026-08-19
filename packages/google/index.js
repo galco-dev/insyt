e1a06c9a1702ff65f38cfe1b40abfd865bc615d7
galco-dev <galcodev@gmail.com>
2026-08-19 14:07:49 +0400

    Restore repo at root + P2 (google package) + P3 start (rules engine) — from project snapshotCreate index.js

---
+++ packages/google/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ...require('./src/scopes'),
+  ...require('./src/connection-state'),
+  ...require('./src/oauth'),
+  ...require('./src/discovery'),
+  ...require('./src/match'),
+  ...require('./src/route'),
+};
